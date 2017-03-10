3bc87c405cfa5eb2f617c9e171cae57c1f4fca89
philh <philharrison99@gmail.com>
2017-03-10 16:26:50 +0000

    Page verfication + writing

--- index.py
+++ index.py
@@ -37,22 +37,11 @@ def reset():
 @app.route('/page/', defaults={'path': ''})
 @app.route('/page/<path:path>')
 def move_page(path):
-    current_bots[session['username']].update_page(int(path))
-    return redirect("/")
-
-def interpret_page(page):
-    split = re.split("<title>", page)
-    title = "<b>" + split[1] + "</b>"
-    split = re.split("<text>", split[2])
-    text = split[1]
-    text = re.sub("\n\n", "<p>", text)
-    text = "<p>" + text
-    choices = re.split("<choice>", split[2])
-    choices.pop(0)
-    choices = [tuple(re.split("<page>", re.sub("\n", "", x))) for x in choices]
-    choices = [tuple([x[0]] + (re.split("<req>", x[1]))) for x in choices]
-    choices = [(x[0], x[1], re.split("/", x[2])) for x in choices]
-    return [title, text, choices]
+    if 'username' in session:
+        current_bots[session['username']].update_page(path)
+        return redirect("/")
+    else:
+        return redirect("/")
 
 if __name__ == "__main__":
 
--- page_manager.py
+++ page_manager.py
@@ -17,12 +17,12 @@ class PageManager:
         choices = [tuple(re.split("<page>", re.sub("\n", "", x))) for x in choices]
         choices = [tuple([x[0]] + (re.split("<req>", x[1]))) for x in choices]
         choices = [(x[0], x[1], re.split("/", x[2])) for x in choices]
-        choices = [x for x in choices if [y for y in x[2] if y in gavbot.traits + gavbot.inventory] == x[2]]
-        self.page_data = [title, text, choices]
-        return self.page_data
+        choices = [x for x in choices if [y for y in x[2] if y in gavbot.traits + gavbot.inventory] == x[2] or x[2] == [""]]
+        page_data = [title, text, choices]
+        return page_data
 
     def load_page(self, gavbot):
-        file_path = "pages/act_" + str(gavbot.current_act) + "/page_" + str(gavbot.current_page) + ".txt"
+        file_path = "pages/act_" + str(gavbot.current_act) + "/" + str(gavbot.current_page) + ".txt"
         with open(file_path, "r") as file:
             raw_text = file.read()
         page_data = self.interpret_page(raw_text, gavbot)
@@ -41,28 +41,37 @@ class Gavbot:
             self.health = 3
             self.pic = "static/images/gavbot.jpg"
             self.health_pic = "static/images/heart.jpg"
-            self.traits = ["logged"] if self.owner else ["None"]
-            self.inventory = ["None"]
+            self.traits = []
+            self.inventory = []
             self.current_act = 1
-            self.current_page = 0
-            self.page_data = self.manager.load_page(self)
+            self.current_page = "intro"
+            self.manual_update_page(self.current_page)
             self.save_gav()
 
-    def update_page(self, page_number):
-        self.current_page = page_number
+    def manual_update_page(self, page_name):
         self.page_data = self.manager.load_page(self)
+        self.valid_choices = [x[1] for x in self.page_data[2]]
         self.save_gav()
+
+    def update_page(self, page_name):
+        if page_name in self.valid_choices:
+            self.current_page = page_name
+            self.page_data = self.manager.load_page(self)
+            self.valid_choices = [x[1] for x in self.page_data[2]]
+            self.save_gav()
+        else:
+            print("NOT VALID PAGE CHOICE")
 
     def reset_gavbot(self):
         self.manager = PageManager()
         self.health = 3
         self.pic = "static/images/gavbot.jpg"
         self.health_pic = "static/images/heart.jpg"
-        self.traits = ["logged"]
-        self.inventory = ["None"]
+        self.traits = []
+        self.inventory = []
         self.current_act = 1
-        self.current_page = 0
-        self.page_data = self.manager.load_page(self)
+        self.current_page = "intro"
+        self.manual_update_page(self.current_page)
         self.save_gav()
 
     def save_gav(self):
@@ -80,8 +89,6 @@ class Gavbot:
 
     def load_gav(self, owner):
         file_name = "saved_gavbots/" + owner + ".json"
-        print(file_name)
-        print("!!!!!!!!!!!!!!!!")
         with open(file_name, "r") as file:
             data = json.loads(file.read())
         self.owner = data["owner"]
@@ -90,7 +97,7 @@ class Gavbot:
         self.inventory = data["inventory"]
         self.current_act = data["act"]
         self.current_page = data["page"]
-        self.page_data = self.manager.load_page(self)
+        self.manual_update_page(self.current_page)
 
 
 if __name__ == "__main__":
